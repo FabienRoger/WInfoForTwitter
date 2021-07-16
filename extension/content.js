5e80e1462cc98963dfd3c6cda01aff11b0adfa10
Fabien Roger <efab.roger@gmail.com>
2021-07-16 22:41:14 +0200

    Make the paragraph show below instead of on a tooltip. Change the look of the button to simple round

--- extension/content.js
+++ extension/content.js
@@ -15,7 +15,12 @@ new MutationObserver(() => {
       })
       console.log(text);
       if(buttonGroup.childNodes.length == 4){
-        buttonGroup.appendChild(getButton(text))
+        
+        let textDisplay = document.createElement("p");
+        textDisplay.innerText = "loading...";
+        textDisplay.style.display = "none";
+        elt.childNodes[0].childNodes[0].childNodes[0].appendChild(textDisplay);
+        buttonGroup.appendChild(getButton(text, textDisplay))
       }
     }
   })
@@ -23,7 +28,7 @@ new MutationObserver(() => {
 
 let buttons = []
 
-function getButton(str){
+function getButton(str, textDisplay){
   var bgColor = document.querySelector('body').style.backgroundColor;
   var trtColorScheme = 'light';
   if(bgColor == 'rgb(255, 255, 255)'){
@@ -39,35 +44,39 @@ function getButton(str){
   let ele = document.createElement('div');
   ele.className = '__focusOnContent '+trtColorScheme;
   ele.innerHTML = ''
-  +'<button class="popuppp myButton" id="'+buttons.length+'">'
-  +'W'
-  +'<span class="popuppptext" id="myPopup">loading</span>'
-  +'</button>';
+  +'<button class="WButton" id="'+buttons.length+'">W</button>';
 
   ele.onclick = function(){
-    let sp = ele.querySelector('span');
-    sp.classList.toggle("show");
-    httpGetAsync(baseURL+'/api/getmultiplesentence?'+encodeURI(str), (response) => {
-      let parsed = JSON.parse(response);
-      console.log(parsed);
-      let id = parsed['id'];
-      let answer = parsed['answer'];
-      sp.innerHTML = '';
-
-      let answersHTML = [];
-      for(let i = 0;i<answer.length;i++){
-        answersHTML[i] = document.createElement('p');
-        answersHTML[i].textContent = answer[i]['sentence'];
-        answersHTML[i].onclick = function(){
-          httpPostAsync(baseURL+'/api/selectedsentence?questionid='+id+'&answerselected='+i, (_) => {});
-          window.open(answer[i]['wikiurl'], '_blank').focus();
+    if(textDisplay.style.display === "block"){
+      textDisplay.style.display = "none";
+    }else{
+      textDisplay.style.display = "block";
+      textDisplay.innerHTML = 'loading...';
+      httpGetAsync(baseURL+'/api/getmultiplesentence?'+encodeURI(str), (response) => {
+        let parsed = JSON.parse(response);
+        console.log(parsed);
+        let id = parsed['id'];
+        let answer = parsed['answer'];
+        textDisplay.innerHTML = '';
+  
+        let answersHTML = [];
+        for(let i = 0;i<answer.length;i++){
+          answersHTML[i] = document.createElement('p');
+          answersHTML[i].className = "clickable";
+          answersHTML[i].textContent = answer[i]['sentence'];
+          answersHTML[i].onclick = function(){
+            httpPostAsync(baseURL+'/api/selectedsentence?questionid='+id+'&answerselected='+i, (_) => {});
+            window.open(answer[i]['wikiurl'], '_blank').focus();
+          }
         }
-      }
+  
+        for(let i = 0;i<answer.length;i++){
+          textDisplay.appendChild(answersHTML[i]);
+        }
+      });
+    }
 
-      for(let i = 0;i<answer.length;i++){
-        sp.appendChild(answersHTML[i]);
-      }
-    });
+    
   }
   buttons.push((str, ele));
   return ele;
